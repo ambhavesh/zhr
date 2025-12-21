6e561800eb62f43b9d83f1b7b383ff78b02e8836
Bhavesh Tiwari <tiwaribhavesh45@gmail.com>
2025-12-21 13:55:23 +0000

    excel upload added

--- app/employee/webapp/controller/List.controller.js
+++ app/employee/webapp/controller/List.controller.js
@@ -1,17 +1,56 @@
 sap.ui.define([
     "employee/controller/App.controller",
-    "sap/m/MessageBox"
-], (Controller, MessageBox) => {
+    "employee/model/models",
+    "sap/m/MessageBox",
+    "sap/m/Dialog",
+    "sap/m/library",
+    "sap/m/Text",
+    "sap/m/Button"
+], (Controller, Models, MessageBox, Dialog, mobileLibrary, Text, Button) => {
     "use strict";
+
+    const ButtonType = mobileLibrary.ButtonType;
+    const DialogType = mobileLibrary.DialogType;
 
     return Controller.extend("employee.controller.List", {
         onInit() {
             this.oUIModel = this.getModel("ui");
-            this.oUIModel.setProperty("/editEnabled", false);
+            this.oUIModel.setData({
+                editEnabled: false,
+                isCreationPending: false
+            });
         },
 
         onRefreshPress() {
             this.getModel().refresh();
+        },
+
+        onUpdateFinished(oEvent) {
+            const oEmployeeTable = oEvent.getSource();
+            const aNewEmployeeExists = oEmployeeTable.getItems().map(item => item.getBindingContext().isTransient());
+            aNewEmployeeExists.includes(true) ?
+                this.oUIModel.setProperty("/isCreationPending", true) :
+                this.oUIModel.setProperty("/isCreationPending", false);
+
+        },
+
+        async onMassUploadPress(){
+            const oView = this.getView();
+            oView.setBusyIndicatorDelay(0);
+            oView.setBusy(true);
+            this.spreadsheetUpload = await this.getOwnerComponent().createComponent({
+                usage: "spreadsheetImporter",
+                async: true,
+                componentData: {
+                    context: this,
+                    readAllSheets: true,
+                    spreadsheetFileName: "Employee.xlsx",
+                    showBackendErrorMessages: true
+                },
+            });
+
+            this.spreadsheetUpload.openSpreadsheetUploadDialog();
+            oView.setBusy(false);
         },
 
         onUpdatePress: async function () {
@@ -26,7 +65,7 @@ sap.ui.define([
                         item.getCells().forEach(cell => {
                             const oBindInfo = cell.getBindingInfo("value");
                             if (oBindInfo) {
-                                cell.setEnabled(false);
+                                cell.setEditable(false);
                             }
                         });
                     });
@@ -47,6 +86,104 @@ sap.ui.define([
             });
         },
 
+        onCreatePress() {
+            const oEmployeeTable = this.byId("employeeTable");
+            const oBinding = oEmployeeTable.getBinding("items");
+            const oPayload = Models._createEmployeePayload();
+            oBinding.create(oPayload);
+            oEmployeeTable.getItems().forEach((item) => {
+                const isTransient = item.getBindingContext().isTransient();
+                if (isTransient) {
+                    item.getCells().forEach(cell => {
+                        if (cell instanceof sap.m.Input) { cell.setEditable(true); }
+                    })
+                } else return;
+            });
+            this.oUIModel.setProperty("/isCreationPending", true);
+        },
+
+        onSavePress() {
+            try {
+                this._onSaveEmployee(this.getModel()).then(() => {
+                    this.oUIModel.setProperty("/isCreationPending", false);
+                    MessageBox.success("Employee saved successfully", { title: "Success" });
+                    const oEmployeeTable = this.byId("employeeTable");
+                    oEmployeeTable.getItems().forEach((item) => {
+                        item.getCells().forEach(cell => {
+                            if (cell instanceof sap.m.Input) { cell.setEditable(false); }
+                        })
+                    });
+                })
+            } catch (error) {
+                MessageBox.error("Employee saving failed", { title: "Error" });
+                this.oUIModel.setProperty("/isCreationPending", false);
+            }
+        },
+
+        _onSaveEmployee(oModel) {
+            return new Promise((resolve, reject) => {
+                if (oModel.hasPendingChanges()) {
+                    oModel.submitChanges({
+                        success: resolve,
+                        error: reject
+                    })
+                }
+            });
+        },
+
+        onDeletePress(oEvent) {
+            const oSource = oEvent.getSource();
+            const oModel = oSource.getModel();
+            const oBindingContext = oSource.getBindingContext();
+            const sPath = oBindingContext.getPath();
+
+            if (oBindingContext.isTransient()) {
+                oBindingContext.delete().then(() => {
+                    this.byId("employeeTable").getItems().forEach(item => {
+                        const oBindingContext = item.getBindingContext();
+                        const bNewEmployee = oBindingContext.isTransient();
+                        if (!bNewEmployee) {
+                            item.getCells().forEach(cell => { if (cell instanceof sap.m.Input) cell.setEditable(false) })
+                        }
+                    })
+                })
+            } else {
+                if (!this.oConfirmationDialog) {
+                    this.oConfirmationDialog = new Dialog({
+                        type: DialogType.Message,
+                        title: "Confirm",
+                        state: "Information",
+                        content: new Text({ text: "Do you want to delete this employee ?" }),
+                        beginButton: new Button({
+                            type: ButtonType.Emphasized,
+                            text: "Delete",
+                            press: async function () {
+                                await this._deleteEmployee(oModel, sPath);
+                                this.oConfirmationDialog.close();
+                            }.bind(this)
+                        }),
+                        endButton: new Button({
+                            text: "Cancel",
+                            press: function () {
+                                this.oConfirmationDialog.close();
+                            }.bind(this)
+                        })
+                    });
+                }
+
+                this.oConfirmationDialog.open();
+            }
+        },
+
+        _deleteEmployee(oModel, sPath) {
+            return new Promise((resolve, reject) => {
+                oModel.remove(sPath, {
+                    success: resolve,
+                    error: reject
+                })
+            })
+        },
+
         onEmployeeSelectionChange(oEvent) {
             const mParams = oEvent.getParameters();
             const bSelected = mParams.selected;
@@ -57,7 +194,7 @@ sap.ui.define([
             aListItems.forEach(item => {
                 item.getCells().forEach(cell => {
                     if (cell.getBindingInfo("value")) {
-                        cell.setEnabled(bSelected);
+                        cell.setEditable(bSelected);
 
                         if (!bSelected) {
                             const oContext = item.getBindingContext();
